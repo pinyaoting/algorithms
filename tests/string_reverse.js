375a34340d5d6c07c0d7466ecd82befc09058d48
pythis <pythis@yahoo.com>
2014-06-17 00:02:04 -0700

    include string reverse

---
+++ tests/string_reverse.js
@@ -0,0 +1,16 @@
+/*jslint indent: 4, nomen:true, white:true */
+/*global require, describe:true, it:true*/
+
+var expect = require('chai').expect,
+    test = require('../src/string_reverse.js');
+
+describe('String Reverse', function () {
+    var module;
+
+    it('should reverse a string', function (done) {
+
+        expect(test.reverse("abcdef")).to.equal("fedcba");
+        done();
+
+    });
+});
