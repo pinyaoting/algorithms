f462c02176efd679e6fb6bfab6866090c229a031
pythis <pythis@yahoo.com>
2014-07-13 08:25:15 -0700

    refactor

---
+++ src/treenode.js
@@ -0,0 +1,11 @@
+/*jslint indent: 4, nomen:true, white:true, continue:true */
+'use strict';
+var TreeNode;
+
+TreeNode = function (item) {
+    this.data = item;
+    this.left = null;
+    this.right = null;
+};
+
+module.exports = TreeNode;
